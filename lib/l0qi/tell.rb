5016d7ad57f1cc8330c462e6eb9809b636bb7d8b
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2015-07-20 11:49:56 -0700

    lpop

--- lib/l0qi/tell.rb
+++ lib/l0qi/tell.rb
@@ -32,7 +32,7 @@ module L0qi
       l = USER_MESSAGES % user.nick
       len = R.llen l
       if len > 0
-        while msg = R.lpop l
+        while msg = R.lpop(l)
           msg = JSON.parse msg
           msg = [
             "#{user}: #{msg['from']} left you a message at #{msg['at']} -",
